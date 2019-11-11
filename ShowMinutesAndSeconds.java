a38adc78418dd8aa294d550a5438826e4ff522f8
AntonioMTS <55556611+AntonioMTS@users.noreply.github.com>
2019-11-11 09:23:01 -0500

    Update ShowMinutesAndSeconds.java

--- ShowMinutesAndSeconds.java
+++ ShowMinutesAndSeconds.java
@@ -1,8 +1,12 @@
 public class ShowMinutesAndSeconds {
+	
 	public static void main(String[] args) {
+		
 		int totalSeconds = 130;
 		int minutes = totalSeconds / 60;
-		int remainingSeconds = totalSeconds % 60; // = totalSeconds - minutes * 60;
+		int remainingSeconds = totalSeconds % 60;
 		System.out.println(minutes + " minutes and " + remainingSeconds + " seconds");
+	
 	}
+	
 }
