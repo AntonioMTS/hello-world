2d422fdca5b2bfc163f30c047517549db57b019c
AntonioMTS <55556611+AntonioMTS@users.noreply.github.com>
2019-11-11 09:22:26 -0500

    Add files via upload

---
+++ ShowMinutesAndSeconds.java
@@ -0,0 +1,8 @@
+public class ShowMinutesAndSeconds {
+	public static void main(String[] args) {
+		int totalSeconds = 130;
+		int minutes = totalSeconds / 60;
+		int remainingSeconds = totalSeconds % 60; // = totalSeconds - minutes * 60;
+		System.out.println(minutes + " minutes and " + remainingSeconds + " seconds");
+	}
+}
